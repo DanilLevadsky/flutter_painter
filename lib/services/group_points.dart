0e7fbcc53b8cb74204852fb149659cee53dddb32
Danil Levadsky <danillevadsky@gmail.com>
2020-12-05 03:42:18 +0200

    made parameters of GroupPoints required

--- lib/services/group_points.dart
+++ lib/services/group_points.dart
@@ -1,9 +1,12 @@
 import 'dart:ui';
 
+import 'package:flutter/foundation.dart';
+
 class GroupPoints {
   Offset offset;
   Color color;
   double size;
 
-  GroupPoints({this.color, this.offset, this.size});
+  GroupPoints(
+      {@required this.color, @required this.offset, @required this.size});
 }
